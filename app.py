3ed9adeb21bd1f132198e575ee462f1df2b56336
najelo <44382915+najelo@users.noreply.github.com>
2026-06-27 14:36:45 -0400

    Update app.py

--- app.py
+++ app.py
@@ -217,7 +217,7 @@ with col_centro:
         except Exception as e: 
             st.error(f"Error al conectar con la configuración de emojis: {e}")
             
- with tab3:
+    with tab3:
         st.subheader("📋 Historial de Transacciones")
         lista_logs = obtener_todos_los_logs(supabase)
         
