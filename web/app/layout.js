d3f2a64ad97937346c403d55cac023b370e4b205
najelo <44382915+najelo@users.noreply.github.com>
2026-07-06 18:11:18 -0400

    Update layout.js

--- web/app/layout.js
+++ web/app/layout.js
@@ -1,7 +1,8 @@
+// web/app/layout.js
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <body style={{ margin: 0, padding: 0, height: '100vh', background: '#0a0a0b' }}>
+      <body style={{ margin: 0, padding: 0, background: '#0a0a0b', color: 'white' }}>
         {children}
       </body>
     </html>
